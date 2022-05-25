3a2db2652ab7fde3ef4a3193e95d652a6ec2221a
Akhil <98938102+AKH1LS@users.noreply.github.com>
2022-05-26 03:19:34 +0530

    Update pmmsg.py

--- MusicPlayer/modules/pmmsg.py
+++ MusicPlayer/modules/pmmsg.py
@@ -1,12 +1,12 @@
 from pyrogram import Client
-from Codexun.tgcalls import client as USER
+from MusicPlayer.tgcalls import client as USER
 from pyrogram import filters
 from pyrogram.types import Chat, Message, User
-from Codexun.config import (
+from MusicPlayer.config import (
     BOT_USERNAME,
 )
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hey 👋 I am the assistant of music bot, didn't have a time to talk with you 🙂 kindly join @TeamCodexun for getting support\n\nPowered by @Codexun")
+  await USER.send_message(message.chat.id,"Hello, I am the assistant of music player, didn't have a time to talk with you 🙂 kindly join @BlueCodeSupport for any query\n\nMade by @AKH1LS.")
   return
